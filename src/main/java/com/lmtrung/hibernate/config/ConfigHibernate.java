652f822282a803a7510898a5c327a7c8eb3785ff
minhtrungle <minhtrungle21599@gmail.com>
2023-04-25 23:39:51 +0700

    Update Configuration

---
+++ src/main/java/com/lmtrung/hibernate/config/ConfigHibernate.java
@@ -0,0 +1,41 @@
+package com.lmtrung.hibernate.config;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
+import java.util.Properties;
+
+public class ConfigHibernate {
+
+    private static final SessionFactory FACTORY;
+
+    // Khai báo khối tĩnh được gọi lần đầu khi lớp được nạp lên và chỉ chạy 1 lần duy nhất và tạo đói tượng duy nhất
+    static {
+        Configuration configuration = new Configuration();
+
+        // Cấu hình các properties
+        Properties properties = new Properties();
+        // "Dialect” để biết được loại database nào đang được sử dụng
+        properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
+        properties.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+        properties.put(Environment.URL, "jdbc:mysql://localhost/csdl");
+        properties.put(Environment.USER, "root");
+        properties.put(Environment.PASS, "root");
+        // Thực thi câu truy vấn thì code java sẽ show sql
+        properties.put(Environment.SHOW_SQL, "true");
+
+
+        configuration.setProperties(properties);
+
+        // registry cung cấp các thông tin cho factory để xây dựng factory
+        ServiceRegistry registry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+        // Từ đối tượng configuration tạo FACTORY
+        FACTORY = configuration.buildSessionFactory(registry);
+    }
+    public static SessionFactory getFACTORY() {
+        return FACTORY;
+    }
+}
