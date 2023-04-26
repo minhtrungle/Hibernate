3be412649a7f887a4e266a4707b078a86741c549
minhtrungle <minhtrungle21599@gmail.com>
2023-04-26 23:36:44 +0700

    Update Configuration

--- src/main/java/com/lmtrung/hibernate/config/ConfigHibernate.java
+++ src/main/java/com/lmtrung/hibernate/config/ConfigHibernate.java
@@ -1,5 +1,6 @@
 package com.lmtrung.hibernate.config;
 
+import com.lmtrung.hibernate.pojo.Category;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
@@ -21,7 +22,7 @@ public class ConfigHibernate {
         // "Dialect” để biết được loại database nào đang được sử dụng
         properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
         properties.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
-        properties.put(Environment.URL, "jdbc:mysql://localhost/csdl");
+        properties.put(Environment.URL, "jdbc:mysql://localhost:3306/hibernate");
         properties.put(Environment.USER, "root");
         properties.put(Environment.PASS, "root");
         // Thực thi câu truy vấn thì code java sẽ show sql
@@ -29,6 +30,8 @@ public class ConfigHibernate {
 
 
         configuration.setProperties(properties);
+        // Cho đối tượng cấu hình biết có persistent class
+        configuration.addAnnotatedClass(Category.class);
 
         // registry cung cấp các thông tin cho factory để xây dựng factory
         ServiceRegistry registry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
