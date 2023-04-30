1dd09ce420f913231e95067a31de898859e27f9e
minhtrungle <minhtrungle21599@gmail.com>
2023-04-30 23:19:05 +0700

    Update Manufacturer

---
+++ src/main/java/com/lmtrung/hibernate/pojo/Manufacturer.java
@@ -0,0 +1,51 @@
+package com.lmtrung.hibernate.pojo;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Set;
+
+@Entity
+@Table(name = "manufacturer")
+public class Manufacturer implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String country;
+
+    // Khi cấu hình ngược lại chỉ cần mappedBy đến thuộc tính khai báo của Manufacturer bên Product
+    @ManyToMany(mappedBy = "manufacturers")
+    private Set<Product> products;
+
+    public Set<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(Set<Product> products) {
+        this.products = products;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
