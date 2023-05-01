f78e664b61faa2d9d2ca49ccddc79cbae5ada01f
minhtrungle <minhtrungle21599@gmail.com>
2023-05-01 23:20:30 +0700

    Update Manufacturer

--- src/main/java/com/lmtrung/hibernate/pojo/Manufacturer.java
+++ src/main/java/com/lmtrung/hibernate/pojo/Manufacturer.java
@@ -7,6 +7,7 @@ import java.util.Set;
 @Entity
 @Table(name = "manufacturer")
 public class Manufacturer implements Serializable {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
