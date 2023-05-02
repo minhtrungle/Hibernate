a661ead5a5f664f0b973373a7e15956252a0922b
minhtrungle <minhtrungle21599@gmail.com>
2023-05-02 22:27:30 +0700

    Update ConfigHibernate

--- src/main/java/com/lmtrung/hibernate/config/ConfigHibernate.java
+++ src/main/java/com/lmtrung/hibernate/config/ConfigHibernate.java
@@ -45,7 +45,9 @@ public class ConfigHibernate {
         // Từ đối tượng configuration tạo FACTORY
         FACTORY = configuration.buildSessionFactory(registry);
     }
+
     public static SessionFactory getFACTORY() {
         return FACTORY;
     }
+
 }
