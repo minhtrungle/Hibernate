db8f43235693cc8d6857fb7107e0f79653d9bb16
minhtrungle <minhtrungle21599@gmail.com>
2023-07-25 23:21:02 +0700

    Update

--- src/main/java/com/lmtrung/hibernate/pojo/Category.java
+++ src/main/java/com/lmtrung/hibernate/pojo/Category.java
@@ -19,7 +19,6 @@ public class Category implements Serializable {
     // @Column(name = "id")
     private int id;
 
-
     // @Column(name = "name", length = 100, nullable = false)
     private String name;
 
