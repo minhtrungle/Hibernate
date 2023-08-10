8fd15f889f6c2ceb61e4707ccee2c3ba620e8665
minhtrungle <minhtrungle21599@gmail.com>
2023-08-10 22:24:06 +0700

    Update

--- src/main/java/com/lmtrung/hibernate/pojo/Product.java
+++ src/main/java/com/lmtrung/hibernate/pojo/Product.java
@@ -45,7 +45,6 @@ public class Product implements Serializable {
 
     private Set<Manufacturer> manufacturers;
 
-
     public Set<Manufacturer> getManufacturers() {
         return manufacturers;
     }
