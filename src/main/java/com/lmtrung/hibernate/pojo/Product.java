932028977cc4fc88b7ae776df48e75126c5714e8
minhtrungle <minhtrungle21599@gmail.com>
2023-08-20 15:34:22 +0700

    Update

--- src/main/java/com/lmtrung/hibernate/pojo/Product.java
+++ src/main/java/com/lmtrung/hibernate/pojo/Product.java
@@ -37,11 +37,11 @@ public class Product implements Serializable {
 
     // Cấu hình ManyToMany
     @ManyToMany
-
-            name = "pro_man", // Bảng trung gian
-            joinColumns = { @JoinColumn(name = "product_id") }, // column trong bảng trung gian trỏ đến khóa chính bảng thứ nhất đang xét
-            inverseJoinColumns = { @JoinColumn(name = "manufacturer_id")} // column trong bảng trung gian trỏ đến khóa chính bảng thứ hai
-    )
+//    private
+//            name = "pro_man", // Bảng trung gian
+//            joinColumns = { @JoinColumn(name = "product_id") }, // column trong bảng trung gian trỏ đến khóa chính bảng thứ nhất đang xét
+//            inverseJoinColumns = { @JoinColumn(name = "manufacturer_id")} // column trong bảng trung gian trỏ đến khóa chính bảng thứ hai
+//    )
 
     private Set<Manufacturer> manufacturers;
 
